66697f70aa47699c655fc4137b57c56852a16b37
eug <eugypalu@gmail.com>
2019-08-06 20:00:37 +0200

    +risolti bug precedenti +aggregator a buon punto

--- BlockLockAggregator.sol
+++ BlockLockAggregator.sol
@@ -2,52 +2,72 @@ pragma solidity ^0.5.6;
 
 import "./Blocklock.sol";
 
+//----------------------Il contratto non deve essere mai chiuso, Non implementare la closecontract---------------------
+
 contract BlocklockAggregator {
 
-    address creator;
+    address creator; //potrebbe non servire
     
+    //tiene traccia del numero di blocklock create. Necessario perchè non tiene in considerazione i reset
     uint256 globalCounter;
 
+    //istanza di blocklock
     Blocklock blockL;
     
-    //address[] blocklockList;
-    
-    //mapping (address => bytes32) addressToId;
-    mapping(address => address[]) doorsUser;
-    mapping(address => address[]) userToDoors;
-    
-    modifier onlyCreator(address _usr){
-        require(_usr == creator);
-        _;
+    /**
+     * Info sulla porta
+     * isActive ci dice se la porta è ancora attiva o meno
+     * userList è la lista di utenti di una certa porta
+     **/
+    struct doorInfo{
+        bool isActive;
+        address doorAddress;
     }
+
+    /**
+     * utenti relativi ad una porta
+     * primo parametro è la porta, il seconondo è l'array di utenti
+     **/
+    //mapping(address => address[]) doorsUser;
+    //Posso prenderlo già da blocklock 
+
+    /**
+     * Porte relative ad un singolo utente
+     * primo parametro è l'address dell'utente, il seconondo è l'array di porte (identificate da struct)
+     **/
+    mapping(address => doorInfo[]) userToDoors;
+    
+    event blockLockAdded(uint256 block, address blocklockAddress, address creator);
 
     constructor() public{
         creator = msg.sender;
     }
 
+    /**
+     * Aggiunge una nuova Blocklock
+     **/
     function addBlocklock(address _blocklockAddress, address _usr) public{
-        //addressToId[_blocklockAddress] = _blocklockId;
-        //blocklockList.push(_blocklockAddress);
-        doorsUser[_blocklockAddress].push(_usr);
-        userToDoors[_usr].push(_blocklockAddress);
+        //doorsUser[_blocklockAddress].push(_usr); //Aggiunge il creator come utente della porta
+        userToDoors[_usr].push(doorInfo(true, _blocklockAddress));
         globalCounter += 1;
+        emit blockLockAdded(block.number, _blocklockAddress, _usr);
     }
 
     function addUser(address _blocklockAddress, address _usr) public{
-        doorsUser[_blocklockAddress].push(_usr);
-        userToDoors[_usr].push(_blocklockAddress);
+        //doorsUser[_blocklockAddress].push(_usr);
+        userToDoors[_usr].push(doorInfo(true, _blocklockAddress));
     }
 
     function deleteUser(address _blocklockAddress, address _usr) public{
-        for(uint i = 0; i < doorsUser[_blocklockAddress].length; i++){
+        /*for(uint i = 0; i < doorsUser[_blocklockAddress].length; i++){
             if(doorsUser[_blocklockAddress][i] == _usr){
                 delete doorsUser[_blocklockAddress][i];
                 break;
             }
-        }
+        }*/
 
         for(uint i = 0; i < userToDoors[_usr].length; i++){
-            if(userToDoors[_usr][i] == _blocklockAddress){
+            if(userToDoors[_usr][i].doorAddress == _blocklockAddress){
                 delete userToDoors[_usr][i];
                 break;
             }
@@ -56,16 +76,19 @@ contract BlocklockAggregator {
 
     function resetBlocklock(address _blocklockAddress, address _usr, bytes32 lockId) public{
         for(uint i = 0; i < userToDoors[_usr].length; i++){
-            if(userToDoors[_usr][i] == _blocklockAddress){
-                delete userToDoors[_usr][i];
+            if(userToDoors[_usr][i].doorAddress == _blocklockAddress){
+                userToDoors[_usr][i].isActive = false;
                 break;
             }
         }
-
-        Blocklock bl = new Blocklock(bytesToString(lockId));
-        doorsUser[_usr].push(address(bl));
-        userToDoors[address(bl)].push(_usr);
         
+        Blocklock bl = new Blocklock(bytesToString(lockId), address(this));
+        //doorsUser[_usr].push(address(bl));
+        userToDoors[_usr].push(doorInfo(true, address(bl)));
+    }
+    
+    function getCounter() public view returns(uint256){
+        return globalCounter;
     }
     
     //Non mi serve il delete, ogni volta che resetto blocklock il vecchio addres non controlla più il dispositivo
@@ -85,9 +108,5 @@ contract BlocklockAggregator {
         }
         return result;
     } 
-    
-    function closeContract() public onlyCreator(msg.sender){
-        selfdestruct(address(0));
-    }
 
 }
--- Blocklock.sol
+++ Blocklock.sol
@@ -1,10 +1,15 @@
 pragma solidity ^0.5.6;
 
+import "./BlocklockAggregator.sol";
+
 contract Blocklock {
     
     address creator;
     bool status;
     bytes32 lockId;
+    BlocklockAggregator aggregator;
+    address[] users;
+    address[] temp;
     
     /**
      * 0 is not user 
@@ -13,7 +18,7 @@ contract Blocklock {
      * se maggiore di 2 allora que valore è il limite temporale oltre il quale non potrà rientrare
      */
     mapping (address => uint256) isRoot;
-    
+
     modifier isUser(address _usr){
         require(isRoot[_usr] == 1 || isRoot[_usr] == 2 || isRoot[_usr] >= block.number);
         _;
@@ -29,62 +34,68 @@ contract Blocklock {
         _;
     }
     
-    event statusChanged(uint256 block);//non metto altre info come l'address di chi ha aperto la porta per non lasciare troppe info 
-    event blocklockCreated(uint256 block);
-    event userAdded(uint256 block);
-    event userUpgraded(uint256 block);
-    event userDeleted(uint256 block);
+    event statusChanged(uint256 block, address blocklockAddress, address user); 
+    event blocklockCreated(uint256 block, address blocklockAddress, address user);
+    event userAdded(uint256 block, address blocklockAddress, address fromUser, address to);
+    event userUpgraded(uint256 block, address blocklockAddress, address fromUser, address to);
+    event userDeleted(uint256 block, address blocklockAddress, address fromUser, address to);
     
-    constructor(string memory _lockId/*, address _creator*/) public{
-        //creator = _creator;
+    constructor(string memory _lockId, address _aggregator) public{
         creator = msg.sender;
+        aggregator = BlocklockAggregator(_aggregator);
         lockId = stringToBytes(_lockId);
-        emit blocklockCreated(block.number);
+        emit blocklockCreated(block.number, address(this), address(creator));
         isRoot[creator] = 2;
+        users.push(creator);
+        aggregator.addBlocklock(address(this), creator);
     }
     
     function changeDoorStatus() isUser(msg.sender) public{
         status = !status;
-        emit statusChanged(block.number);
+        emit statusChanged(block.number, address(this), msg.sender);
     }
     
     function addUser(address _newUSer) onlyRoot(msg.sender) public{
         isRoot[_newUSer] = 1;
-        emit userAdded(block.number);
+        emit userAdded(block.number, address(this), msg.sender, _newUSer);
+        users.push(_newUSer);
     }
     
     function addTimeUser(address _newUSer, uint256 _duration) onlyRoot(msg.sender) public{
         isRoot[_newUSer] = block.number + _duration;
-        emit userAdded(block.number);
+        emit userAdded(block.number, address(this), msg.sender, _newUSer);
+        users.push(_newUSer);
+        aggregator.addUser(address(this), _newUSer); //questo è da gestire, perchè quando è scaduto il tempo andrebbe eliminato dall'aggregator
     }
     
     function upgradeUser(address _stdUser) onlyRoot(msg.sender) public{
         isRoot[_stdUser] = 2;
-        emit userUpgraded(block.number);
+        emit userUpgraded(block.number, address(this), msg.sender, _stdUser);
     }
     
-    /**
-     * a differenza della precedente versione il creator ha i poteri supremi :)
-     * può eliminare chiunque
-     * (da ragionarci)
-     * Se questa funzione la lasciamo così non serve nemmeno la resetDoor, perchè l'unico che ha il permesso di resettarla
-     * è il creator, quindi tanto vale che chiude il contratto e lo rideploya(meno costoso)
-     * In alternativa tenere traccia degli utenti su array poi elminare tutti gli address del map scorrendo l'array
-     */
-    function deleteUser(address _usr) onlyRoot(msg.sender) public{//non vera la cosa del creator i root eliminano i root
-    //TODO cambiare tutto
-        if(isRoot[_usr] == 1){
-           isRoot[_usr] = 0; 
-            emit userDeleted(block.number);
-        }else if(isRoot[_usr] == 2){
-            require(msg.sender == creator);
-            isRoot[_usr] = 0; 
-            emit userDeleted(block.number);
-        }
+    function deleteUser(address _usr) onlyRoot(msg.sender) public{
+        isRoot[_usr] = 0;
+        emit userDeleted(block.number, address(this), msg.sender, _usr);
+        aggregator.deleteUser(address(this), _usr);
+    }
+    
+    function resetLock() public onlyRoot(msg.sender){
+        aggregator.resetBlocklock(address(this), msg.sender, lockId);
     }
     
     function getDoorStatus() public view returns(bool){
         return status;
+    }
+    
+    function getUser() public view returns(address[] memory){
+        address[] memory res;
+        uint count;
+        for(uint i = 0; i < users.length; i++){
+            if(isRoot[users[i]] == 1 || isRoot[users[i]] == 2){
+                res[count] = users[i];
+            }
+        }
+        return res;
     }
     
     function stringToBytes(string memory _s) private pure returns (bytes32){
